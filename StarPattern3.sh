098730cd2c9a4aa1fca2632f31b1acfca4e2cb22
sudha sanshi <35460293+sudhasanshi@users.noreply.github.com>
2024-02-03 19:17:29 +0530

    Create StarPattern3.sh

---
+++ StarPattern3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for (( i=5; i>=1; i-- ))
+do
+        for (( j=1; j<=i; j++ ))
+        do
+                echo -n "*"
+        done
+        echo " "
+done
+
+
+
+output
+***** 
+**** 
+*** 
+** 
+*
