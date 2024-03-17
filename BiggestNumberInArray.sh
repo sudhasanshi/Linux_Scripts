e4abdb10d37928ef5165dcfb81e845f4593b4eb7
sudha sanshi <35460293+sudhasanshi@users.noreply.github.com>
2024-03-17 22:33:19 +0530

    Update BiggestNumberInArray.sh

--- BiggestNumberInArray.sh
+++ BiggestNumberInArray.sh
@@ -11,3 +11,17 @@ do
 done
 echo "Biggest value in Array is $Big"
 	
+
+
+#!/bin/bash
+arr="1 2 4 5"
+values=($arr)
+big=${values[0]}
+for i in ${values[@]}
+do
+        if [ $i -gt $big ]
+        then
+                big=$i
+        fi
+done
+echo "Biggest value in Array is $big"
